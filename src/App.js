96a05d65dc51147d0473b37ff63e0f2f345c1825
Tony Lauf <LAU5619@tonys-mbp.nm.nmfco.com>
2018-02-05 12:30:32 -0600

    need to highlight selected nav item yet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './ui-toolkit/css/nm-cx/main.css'
+import "react-router";
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <div>
+            <ul className="heading-nav">
+              <li className="heading-nav-entry"><Link to="/">Home</Link></li>
+              <li className="heading-nav-entry"><Link to="/users">Users</Link></li>
+              <li className="heading-nav-entry"><Link to="/todos">Todos</Link></li>
+            </ul>
+            <Route exact path="/" component={Home} />
+            <Route path="/users" component={Users} />
+            <Route path="/todos" component={Todos} />
+          </div>
+        </BrowserRouter>
       </div>
     );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+        <div class="row">&nbsp;</div>
+          <div class="row">
+            <div class="small-5 columns">&nbsp;</div>
+            <div class="small-2 columns">
+            <div class="card text-center">
+              <h1>Users<br/>and<br/> Todos</h1>
+        </div>
+            </div>
+            <div class="small-5 columns">&nbsp;</div>
+          </div>
+      </div>
+    )
+  }
+}
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>User 1</li>
+          <li>User 2</li>
+          <li>User 3</li>
+          <li>User 4</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Todo 1</li>
+          <li>Todo 2</li>
+          <li>Todo 3</li>
+          <li>Todo 4</li>
+        </ul>
+      </div>
+    )
   }
 }
 
